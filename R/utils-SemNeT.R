20e86a62992c410b9b889767d3fe08ea8b666548
Alexander Christensen <alexpaulchristensen@gmail.com>
2020-11-30 11:47:25 -0500

    updated startup message

--- R/utils-SemNeT.R
+++ R/utils-SemNeT.R
@@ -1579,3 +1579,76 @@ binarize <- function (A)
   return(bin)
 }
 #----
+
+#' Stylizes Text
+#' 
+#' Makes text bold, italics, underlined, and strikethrough
+#' 
+#' @param text Character.
+#' Text to stylized
+#' 
+#' @return Sytlized text
+#' 
+#' @author Alexander Christensen <alexpaulchristensen@gmail.com>
+#' 
+#' @noRd
+# Style text----
+# Updated 08.09.2020
+styletext <- function(text, defaults = c("bold", "italics", "highlight",
+                                         "underline", "strikethrough"))
+{
+  # Check system
+  sys.check <- system.check()
+  
+  if(sys.check$TEXT)
+  {
+    if(missing(defaults))
+    {number <- 0
+    }else{
+      
+      # Get number code
+      number <- switch(defaults,
+                       bold = 1,
+                       italics = 3,
+                       underline = 4,
+                       highlight = 7,
+                       strikethrough = 9
+      )
+      
+    }
+    
+    return(paste("\033[", number, ";m", text, "\033[0m", sep = ""))
+  }else{return(text)}
+}
+
+#' System check for OS and RSTUDIO
+#' 
+#' @description Checks for whether text options are available
+#' 
+#' @param ... Additional arguments
+#' 
+#' @return \code{TRUE} if text options are available and \code{FALSE} if not
+#' 
+#' @author Alexander Christensen <alexpaulchristensen@gmail.com>
+#' 
+#' @noRd
+# System Check----
+# Updated 08.09.2020
+system.check <- function (...)
+{
+  OS <- unname(tolower(Sys.info()["sysname"]))
+  
+  RSTUDIO <- ifelse(Sys.getenv("RSTUDIO") == "1", TRUE, FALSE)
+  
+  TEXT <- TRUE
+  
+  if(!RSTUDIO){if(OS != "linux"){TEXT <- FALSE}}
+  
+  res <- list()
+  
+  res$OS <- OS
+  res$RSTUDIO <- RSTUDIO
+  res$TEXT <- TEXT
+  
+  return(res)
+}
--- R/zzz.R
+++ R/zzz.R
@@ -3,7 +3,8 @@
 
 .onAttach <- function(libname, pkgname)
 {
-    msg <- paste('\nFor help getting started, see <https://doi.org/10.31234/osf.io/eht87> \n')
+    msg <- styletext(styletext("\nSemNeT", defaults = "underline"), defaults = "bold")
+    msg <- paste(msg,'\nFor help getting started, see <https://doi.org/10.31234/osf.io/eht87> \n')
     msg <- paste(msg,"For bugs and errors, submit an issue to <https://github.com/AlexChristensen/SemNeT/issues>\n")
     msg <- paste(msg,"For a GUI experience, check out the Shiny app: SemNeTShiny()\n")
     packageStartupMessage(msg)
