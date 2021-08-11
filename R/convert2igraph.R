a254a9eb5340ff3d6d6fa5498f74f21c41479dd6
Alexander Christensen <alexpaulchristensen@gmail.com>
2021-08-11 14:43:00 -0400

    Update convert2igraph.R

--- R/convert2igraph.R
+++ R/convert2igraph.R
@@ -4,13 +4,7 @@
 #' @param A Adjacency matrix (network matrix) or brain connectivity array
 #' (from \code{\link[NetworkToolbox]{convertConnBrainMat}})
 #' 
-#' @param neural Is input a brain connectivity array (i.e., m x m x n)?
-#' Defaults to \code{FALSE}.
-#' Set to \code{TRUE} to convert each brain connectivity matrix
-#' 
-#' @return Returns a network matrix in \code{\link{igraph}}'s format or
-#' returns a list of brain connectivity matrices each of which have been
-#' convert to \code{\link{igraph}}'s format
+#' @return Returns a network matrix in \code{\link{igraph}}'s format
 #' 
 #' @examples
 #' # Pearson's correlation only for CRAN checks
@@ -28,29 +22,11 @@
 #' 
 #' @export
 #Convert matrices to igraph format----
-# Updated 25.09.2020
-convert2igraph <- function (A, neural = FALSE)
+# Updated 09.08.2021
+convert2igraph <- function (A)
 {
-    if(!neural)
-    {
-        net <- igraph::as.igraph(qgraph::qgraph(A,DoNotPlot=TRUE))
-        return(net)
-    }else if(neural)
-    {
-        netlist <- list()
-        
-        n<-length(A)/nrow(A)/ncol(A)
-        
-        pb <- txtProgressBar(max=n, style = 3)
-        
-        for(i in 1:n)
-        {
-            netlist[[i]] <- igraph::as.igraph(qgraph::qgraph(A[,,i],DoNotPlot=TRUE))
-            setTxtProgressBar(pb, i)
-        }
-        close(pb)
-        
-        return(netlist)
-    }
+    net <- igraph::as.igraph(qgraph::qgraph(A,DoNotPlot=TRUE))
+    igraph::vertex_attr(net, "name") <- V(net)$label
+    return(net)
 }
 #----
