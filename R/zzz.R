9057ecc19e84438c3ebfed5e805f4d4abdf6248a
Alexander Christensen <alexpaulchristensen@gmail.com>
2022-01-21 13:05:23 -0500

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -3,7 +3,8 @@
 
 .onAttach <- function(libname, pkgname)
 {
-    msg <- styletext(styletext("\nSemNeT", defaults = "underline"), defaults = "bold")
+    msg <- styletext(styletext(paste("\nSemNeT (version ", packageVersion("SemNeT"), ")", sep = ""), defaults = "underline"), defaults = "bold")
+    
     msg <- paste(msg,'\nFor help getting started, see <https://doi.org/10.1037/met0000463> \n')
     msg <- paste(msg,"For bugs and errors, submit an issue to <https://github.com/AlexChristensen/SemNeT/issues>\n")
     msg <- paste(msg,"For a GUI experience, check out the Shiny app: SemNeTShiny()\n")
